e04f5a76aac43f1ab001eb2f19c5698f5fbaa8ca
Justin Hays <jhays124@yahoo.com>
2018-02-13 21:28:20 -0800

    starting login route

--- js/src/login.js
+++ js/src/login.js
@@ -2,7 +2,11 @@ const usersURL = 'http://localhost:3000/users/'
 
 function login(email, password) {
   console.log('trying to log in jack?', email, password)
-  let 
+  let body = {email, password}
   //this stuff needs to be compared to db
-  axios.get(`${usersURL}`)
+  axios.post(`${usersURL}`, body)
+    .then((data) => {
+
+    })
+    .catch(err => console.error(err))
 }
